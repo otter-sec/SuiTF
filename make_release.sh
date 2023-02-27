866525dc5bb4a078142e8f8128ac65d99e6d9398
embe221ed <baniak996@gmail.com>
2023-02-27 22:53:55 +0100

    Update the make_release script

--- make_release.sh
+++ make_release.sh
@@ -1,7 +1,17 @@
 set -eux
 
-COPYFILE_DISABLE=true tar --exclude-from=.gitignore -hczvf release.tar.gz release
 rm -rf output
 mkdir output
-mv release.tar.gz ./output
-cd output && tar xzvf release.tar.gz
+
+cd release
+
+for CHALL in {avalanche_alert,mile_high_city,your_secret_is_safe_with_us}; do
+  COPYFILE_DISABLE=true tar --exclude-from=../.gitignore -hczvf $CHALL.tar.gz $CHALL
+  mv $CHALL.tar.gz ../output/
+done
+
+cd ../output
+
+for CHALL in {avalanche_alert,mile_high_city,your_secret_is_safe_with_us}; do
+  tar xzvf $CHALL.tar.gz
+done
